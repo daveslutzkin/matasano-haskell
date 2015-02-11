3f55f026004874c26683c21902d23edc28196cd5
Dave Slutzkin <daveslutzkin@fastmail.fm>
2015-02-11 14:04:38 +1100

    Tidy up challenge1 code style.

--- challenge1/main.hs
+++ challenge1/main.hs
@@ -1,12 +1,12 @@
 import System.Environment (getArgs)
-import qualified Data.ByteString.Char8 as Char8
-import qualified Data.ByteString.Base16 as Base16
-import qualified Data.ByteString.Base64 as Base64
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as Char8 (pack, unpack)
+import qualified Data.ByteString.Base16 as Base16 (decode)
+import qualified Data.ByteString.Base64 as Base64 (encode)
 
-changeBase16to64 base16chars = do
-    let (bytes, _) = Base16.decode base16chars
-    Base64.encode bytes
+changeBase16to64 :: String -> String
+changeBase16to64 = Char8.unpack . Base64.encode . fst . Base16.decode . Char8.pack
 
 main = do
     args <- getArgs
-    print (changeBase16to64 (Char8.pack (args !! 0)))
+    print $ changeBase16to64 $ args !! 0
